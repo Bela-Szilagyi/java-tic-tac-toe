4c61bbaf1c96766dcda3abc00cf19b3f720bcca3
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-08-09 21:53:16 +0200

    checkwin

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,103 +5,190 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
-
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    private String[] gameState = {"-", "-", "-", "-", "-", "-", "-", "-", "-"};
 
+    Player playerX = new Player("Player", 0);
+    Player playerO = new Player("Player", 0);
+
+    class buttonActionHandler implements EventHandler<ActionEvent> {
+
+        private final int number ;
+
+        buttonActionHandler(int number) {
+            this.number = number ;
+        }
+        @Override
+        public void handle(ActionEvent event) {
+            System.out.println("Event " + number);
+            Button temp_button = ((Button)event.getSource());
+            if( temp_button.getText() == "" ){
+            String mark = player.get(starter);
+            System.out.println("Player: " + mark);
+            temp_button.setText(mark);
+            starter = !starter;
+            System.out.println("Round " + round_counter);
+            gameState[number - 1] = mark;
+//
+//          //TODO: Win check from here
+            if (round_counter >= 5) {
+                boolean win = checkWin(mark, gameState);
+                System.out.println("checkwin: " + win);
+                String name = null;
+                if (win) {
+                    if (mark.equals("X")) {
+                        name = playerX.getPlayerName();
+                    } else {
+                        name = playerO.getPlayerName();
+                    }
+                    alertGameOver(win, name);
+                } else if (round_counter == 9) {
+                    alertGameOver(win, name);
+                }
+            }
+            round_counter++;
+            }
+        }
+    }
+
+    public static void alertGameOver(boolean win, String name) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Game over!");
+        if (win) {
+            alert.setHeaderText("The winner is: ");
+            alert.setContentText(name);
+        } else {
+            alert.setHeaderText("It is a tie!");
+            alert.setContentText("Tie!");
+        }
+        alert.showAndWait();
+    }
+
+    Map<Boolean, String> player= new HashMap<>();
+
+    private boolean starter = firstPlayer();
+    private int round_counter;
+
+    private Button createGridButton(int number) {
+        Button button = createButton(Integer.toString(number));
+        button.setOnAction(new buttonActionHandler(number));
+        return button ;
+    }
+
+    private Button createButton(String number) {
         Button button = new Button();
-        Button button2 = new Button();
-        Button button3 = new Button();
-        Button button4 = new Button();
-        Button button5 = new Button();
-        Button button6 = new Button();
-        Button button7 = new Button();
-        Button button8 = new Button();
-        Button button9 = new Button();
-
         // set (CSS) id for element
-        button.setId("b1");
-
+        button.setId(number);
         button.setPrefWidth(100);
         button.setPrefHeight(100);
+        return button ;
+    }
 
-        button2.setPrefWidth(100);
-        button2.setPrefHeight(100);
+    @Override
+    public void start(Stage primaryStage) throws Exception{
 
-        button3.setPrefWidth(100);
-        button3.setPrefHeight(100);
+        //set the players
+        TextInputDialog playerXInput = new TextInputDialog("Name...");
+        playerXInput.setTitle("Player X name");
+        playerXInput.setHeaderText("Player X, type your name!");
+        playerXInput.setContentText("Name:");
+        Optional<String> inputX = playerXInput.showAndWait();
+        if (inputX.isPresent()) {
+            playerX.setPlayerName(inputX.get());
+        }
+        TextInputDialog playerOInput = new TextInputDialog("Name...");
+        playerOInput.setTitle("Player O name");
+        playerOInput.setHeaderText("Player O, type your name!");
+        playerOInput.setContentText("Name:");
+        Optional<String> inputO = playerOInput.showAndWait();
+        if (inputO.isPresent()) {
+            playerO.setPlayerName(inputO.get());
+        }
+        Tie tie = new Tie(0);
 
-        button4.setPrefWidth(100);
-        button4.setPrefHeight(100);
+        round_counter = 1;
 
-        button5.setPrefWidth(100);
-        button5.setPrefHeight(100);
+        player.put(true, "X");
+        player.put(false, "O");
 
-        button6.setPrefWidth(100);
-        button6.setPrefHeight(100);
+        /////////////////////////////////////////////////////////
 
-        button7.setPrefWidth(100);
-        button7.setPrefHeight(100);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("First player info");
+        String mark = player.get(starter);
+        alert.setHeaderText("The first player is: " + mark);
+        String firstPlayerName;
+        if (mark == "X") {
+            firstPlayerName = playerX.getPlayerName();
+        } else {
+            firstPlayerName = playerO.getPlayerName();
+        }
+        alert.setContentText(firstPlayerName);
+        alert.showAndWait();
 
-        button8.setPrefWidth(100);
-        button8.setPrefHeight(100);
+        /////////////////////////////////////////////////////////
 
-        button9.setPrefWidth(100);
-        button9.setPrefHeight(100);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        // set button action
-        /*
-        List P1Win = new ArrayList<>();
-
-        button.setOnAction(e -> {
-            button.setText("O");
-            P1Win.add(0);
-        });
-        */
 
         GridPane rootBoard = new GridPane();
         rootBoard.setMinSize(300,324);
 
         GridPane gameBoard = new GridPane();
-        gameBoard.add(button, 1, 1);
-        gameBoard.add(button2, 2, 1);
-        gameBoard.add(button3, 3, 1);
-        gameBoard.add(button4, 1, 2);
-        gameBoard.add(button5, 2, 2);
-        gameBoard.add(button6, 3, 2);
-        gameBoard.add(button7, 1, 3);
-        gameBoard.add(button8, 2, 3);
-        gameBoard.add(button9, 3, 3);
+        // CREATE BUTTON
+        for (int n = 1; n<10; n++) {
+            Button button = createGridButton(n);
+            int row = (n-1) / 3;
+            int col = (n-1) % 3;
+            gameBoard.add(button, col, row);
+        }
+
+        // set (CSS) id for element
+
+
+        // set button action
+//        List P1Win = new ArrayList<>();
+
+//        button.setOnAction(new buttonHandler(number));
         gameBoard.setMinSize(300,300);
 
         GridPane gameScore = new GridPane();
         gameScore.setMinSize(300, 24);
 
+
+        String playerXName = playerX.getPlayerName();
+        int playerXScore = playerX.getPlayerScore();
+
         Label player1Label = new Label();
         player1Label.setMinSize(100, 24);
         player1Label.setAlignment(Pos.CENTER);
-        player1Label.setText("Player X:");
+        player1Label.setText(playerXName+" X: "+playerXScore);
 
         Label player2Label = new Label();
         player2Label.setMinSize(100, 24);
         player2Label.setAlignment(Pos.CENTER);
-        player2Label.setText("Player O:");
+        player2Label.setText(playerO.getPlayerName() +" O: " + playerO.getPlayerScore());
 
         Label tieLabel = new Label();
         tieLabel.setMinSize(100, 24);
         tieLabel.setAlignment(Pos.CENTER);
-        tieLabel.setText("Tie:");
+        tieLabel.setText("Tie: "+ tie.getTieScore());
 
 
         gameScore.add(player1Label, 1, 1);
@@ -118,8 +205,28 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+
         launch(args);
+
+
+    }
+
+    public static boolean firstPlayer() {
+        return Math.random() < 0.5;
+    }
+
+
+    private static boolean checkWin(String player, String[] game) {
+        int[][] winnerCombos = new int[][] {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}
+        };
+        for (int[] combination : winnerCombos) {
+            if (game[combination[0]].equals(player) && game[combination[1]].equals(player) && game[combination[2]].equals(player)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
